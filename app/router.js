1c452a007b45ce58e04f9dc4d6ef53cafbd728de
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-21 10:06:33 -0400

    fast

--- app/router.js
+++ app/router.js
@@ -10,6 +10,10 @@ Router.map(function () {
   this.route('sign-in');
   this.route('change-password');
   this.route('users');
+  this.route('customers', function() {
+    this.route('customer', { path: ':customer_id'}); //customer/3
+    this.route('new');
+  });
 });
 
 export default Router;
