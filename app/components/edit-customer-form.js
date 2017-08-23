c200850f4be7ca0ea33074c42a5c8d7345014f67
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-23 11:07:54 -0400

    modified edit route and edit-customer-form.js

--- app/components/edit-customer-form.js
+++ app/components/edit-customer-form.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    editCustomer () {
+      this.sendAction('editCustomer', this.get('customer'));
+    }
+  }
 });
--- app/routes/edit.js
+++ app/routes/edit.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model (params) {
+    return this.get('store').findRecord('customer', params.customer_id);
+  },
+  actions: {
+   editCustomer (customer) {
+      console.log(customer.title)
+      list.save()
+      this.transitionTo('customers')
+    }
+  }
 });
