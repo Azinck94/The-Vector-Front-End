1d073cf0f74a41c05b8cff5fa47414303c8f789a
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-25 14:32:20 -0400

    create-form-customer-js

--- app/components/create-form.js
+++ app/components/create-form.js
@@ -3,12 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   newCustomer: {
     name: null,
+    email: null,
+    phone: null,
     hidden: false
   },
   actions: {
     createCustomer () {
       this.sendAction('createCustomer', this.get('newCustomer'))
       this.set('newCustomer.name', null);
+      this.set('newCustomer.phone', null);
+      this.set('newCustomer.email', null);
      }
   }
 });
