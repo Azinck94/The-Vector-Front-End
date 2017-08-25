f901acb67030122456088817a2a7b70dc332bb18
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-25 16:26:37 -0400

    update

--- app/components/edit-customer-form.js
+++ app/components/edit-customer-form.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   actions: {
     editCustomer () {
       this.sendAction('editCustomer', this.get('customer'));
+      this.set('customer.name', null);
+      this.set('customer.phone', null);
+      this.set('customer.email', null);
     }
   }
 });
