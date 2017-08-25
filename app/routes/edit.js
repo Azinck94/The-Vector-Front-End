b507ee0925bafc7059c9363d7f4a135810d3d594
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-25 14:22:04 -0400

    edit.js

--- app/routes/edit.js
+++ app/routes/edit.js
@@ -1,13 +1,14 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
   model (params) {
     return this.get('store').findRecord('customer', params.customer_id);
   },
   actions: {
-   editExample (example) {
+   editCustomer (customer) {
       customer.save();
-      this.transitionTo('examples');
+      this.transitionTo('customer');
     }
   }
 });
