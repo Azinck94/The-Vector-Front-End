b9d17aebc921e680853fdf1eb641c8a71717b496
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-25 14:19:56 -0400

    routescustomer

--- app/routes/customer.js
+++ app/routes/customer.js
@@ -4,18 +4,18 @@ export default Ember.Route.extend({
   model (params) {
     return this.get('store').findRecord('customer', params.customer_id);
   },
-
-  actions: {
-   toggleInfoDone (info) {
-      info.toggleProperty('done');
-      info.save();
-    },
-    deleteInfo (info) {
-      info.destroyRecord();
-    },
-    createInfo (info) {
-      let infoRecord = this.get('store').createRecord('info', info);
-      infoRecord.save();
-     }
-  },
-});
+//
+//   actions: {
+//    toggleInfoDone (info) {
+//       info.toggleProperty('done');
+//       info.save();
+//     },
+//     deleteInfo (info) {
+//       info.destroyRecord();
+//     },
+//     createInfo (info) {
+//       let infoRecord = this.get('store').createRecord('info', info);
+//       infoRecord.save();
+//      }
+//   },
+// });
