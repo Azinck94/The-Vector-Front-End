aed03e6865c219fa3547e13cce53074b4222dfea
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-28 10:28:44 -0400

    Adds flash messaging for both success and failure for editing customers
    -Adds a flash message to the edit.js route for a successful edit
    -Adds a flash message to the edit.js route for a edit failure
    -Styled up the edit customer form to make it look fly

--- app/routes/edit.js
+++ app/routes/edit.js
@@ -2,13 +2,21 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
+
   model (params) {
     return this.get('store').findRecord('customer', params.customer_id);
   },
   actions: {
    editCustomer (customer) {
-      customer.save();
-      this.transitionTo('customers');
+      customer.save()
+      .then(() => {
+        this.get('flashMessages').warning('Customer Edited');
+      })
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('You dont have permission to edit this customer');
+      });
+      // this.transitionTo('customers');
     }
   }
 });
