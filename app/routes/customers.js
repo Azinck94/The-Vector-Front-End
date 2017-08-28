9cd73f0d5435de52cbdb1ceabd9d788b7ec1cb28
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-28 10:17:43 -0400

    Adds flash messaging for customer create action
    -Adds a successful flash message when a user creates a new customer.

--- app/routes/customers.js
+++ app/routes/customers.js
@@ -1,14 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  flashMessages: Ember.inject.service(),
+
   model (params) {
     return this.get('store').findAll('customer');
   },
   actions: {
     createCustomer (customer) {
       let newCustomer = this.get('store').createRecord('customer', customer);
-        newCustomer.save();
+        newCustomer.save()
+        .then(() => {
+          this.get('flashMessages').warning('Customer Added')
+        })
     },
+
     deleteCustomer (customer) {
       customer.destroyRecord();
      }
